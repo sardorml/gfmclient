d0332ecc42b03f39198546c7256f96d157c3befd
Sardor <sardormamadaliyev@gmail.com>
2018-07-04 19:28:27 +0900

    client

--- app.js
+++ app.js
@@ -1,91 +1,69 @@
+var request = require('request');
 var express = require('express');
 var bodyParser = require('body-parser');
+const Promise = require('bluebird');
 
-var app = express();
-const PORT = process.env.PORT || 8080;
-var urlencodedParser = bodyParser.urlencoded({extended: false});
+let app = express();
 
+const PORT = process.env.PORT || 4500;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-////////
 var input;
 var result;
-var extra;
-var one_third;
-var for_each;
-///////
-var rem;
-var whole;
-///////
 
-var calculate = function(input){
-	if(input.number>1){
-		if(input.amount < 5000){ // if less than 5000
-			result = input.amount;
-			for_each = 0;
-		}
-		else{
-			one_third = input.amount/3;
-			console.log('One third: ', one_third);
-			if((one_third/(input.number-1))>10000){
-				extra = one_third-(10000*(input.number-1));
-				for_each = 10000;
-				console.log('Extra Amount: ', extra);
-
-				result = (2*one_third)+extra;
-			}else{
-				result = one_third*2; // Pay 2/3
-				for_each = one_third/(input.number-1)
-					
+var newRequest = function(input){
+	return new Promise(function(resolve,reject){
+		request(`http://localhost:8080/api/${input.number}/${input.amount}`, (err, res, body) => {
+		  if (err) { reject(err); }
+			else{
+				result = body;
+				resolve(JSON.parse(result))
+				console.log('body is',body);
 			}
-		}
-	}
-	else{
-		result = input.amount;
-		for_each = 0;
-	}
-	input.result_for_each = Math.round(for_each*100)/100;
-	input.amount = Math.round(result*100)/100;
-	console.log('Number of people in the group: ', input.number);
-	console.log('Total amount need to be paid by you: ', input.amount);
-	console.log('Total amount need to be paid by other members: ', input.result_for_each);
+		});
+	});
 }
-
-var roundIt = function(input){
-	if(input.result_for_each<10000 && input.result_for_each>1000){
-		rem = input.result_for_each%1000;
-		whole = input.result_for_each-rem;
-
-		input.result_for_each = whole;
-		input.amount = input.amount +((input.number-1)*rem)
-		input.amount = Math.round(input.amount);
-	}
+var roundRequest = function(input){
+	return new Promise(function(resolve,reject){
+		request(`http://localhost:8080/api/round/${input.number}/${input.amount}`, (err, res, body) => {
+		  if (err) { return console.log(err); }
+			result = body;
+			resolve(JSON.parse(result));
+		});
+	});
 }
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req,res){
-
-	res.render('home',{obj: req.query});
-});
-app.get('/round', function(req,res){
-	roundIt(input);
-	res.render("results",{obj: input})
+app.get('/',(req,res)=>{
+	res.render('home');
 });
 
+app.get('/round', (req,res)=>{
+	roundRequest(input).then(function(obj) {
+        result = obj;
+				res.render('results',{obj:result});
+    }, function(err) {
+        console.log(err);
+    });
+});
+app.get('/results', (req,res)=>{
+	console.log('final result is',result);
+	res.render("results",{obj: result});
+});
 
-app.post('/',urlencodedParser, function(req,res){
+app.post('/calculate', (req,res)=>{
 	input = req.body;
-	input.amount = parseFloat(input.amount);
-	console.log('User input: ', input);
-	if(input.amount){ // Check if input amount is number
-		calculate(input);
-		res.render('results',{obj: input});
-	}
-	else res.render('error');
-	
-	
-
-	
+	newRequest(input).then(function(obj) {
+        result = obj;
+        console.log("Initialized user details");
+        // Use user details from here
+        // console.log(obj)
+				res.render('results',{obj:result});
+    }, function(err) {
+        console.log(err);
+    });
 });
 
 
